966acc77ff41e7357da703283b2859e7b3b96200
Harsh_Raj <110251286+CODE-ops-stack@users.noreply.github.com>
2024-08-25 11:32:18 +0000

    datatype summary

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,34 @@
+// let score ="33anc"
+// // let score =null
+// let score =undefined
+// let score = "harsh"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber =Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+//"33"=> 33
+//"33aabc"=>NaN
+//true=>1;false=>0;
+
+
+let isLoggedIn =1
+
+let booleanIsLogeedIn =Boolean(isLoggedIn)
+console.log(booleanIsLogeedIn);
+
+
+// *******************Operations ******//
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+
+let str1 ="hitesh"
+let str2="hello"
+let str3 =str1+str2
+console.log(gamecounter)
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+//primitive
+
+
+// 7types :  string,number,boolean,null,undefined,symbol,bigInt
+
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn =false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id===anotherId);
+const bigNumber = 346546464646738n
+
+
+// Refrence (non primitive)
+
+// Array ,Objects, Functions
+
+const heros =["shakti", " naagraz","doga"];
+let myobj = {
+    name:"harsh",
+    age:23,
+
+} 
+const myFunction = function(){
+    console.log("Hello world");
+
+}
+console.log(typeof bigNumber);
+console.log(typeof null)
+
+
